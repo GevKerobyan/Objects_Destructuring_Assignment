2d090c91a46f0b3f685c4759fb07520a4a196e92
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2021-12-15 22:13:51 +0400

    Added solution to the 7th assignment and a yet empty README file

--- classWork10_07.js
+++ classWork10_07.js
@@ -1,6 +1,12 @@
 // Given an object containing counts of both upvotes and downvotes, return what vote count should be
 // displayed. This is calculated by subtracting the number of downvotes from upvotes.
 
-getVoteCount({ upvotes: 13, downvotes: 0 }); // ➞ 13
-getVoteCount({ upvotes: 2, downvotes: 33 }); // ➞ -31
-getVoteCount({ upvotes: 132, downvotes: 132 }); // ➞ 0
+console.log(getVoteCount({ upvotes: 13, downvotes: 0 })); // ➞ 13
+console.log(getVoteCount({ upvotes: 2, downvotes: 33 })); // ➞ -31
+console.log(getVoteCount({ upvotes: 132, downvotes: 132 })); // ➞ 0
+
+function getVoteCount (obj) {
+    let calc = obj.upvotes - obj.downvotes;
+    return calc;
+    console.log(calc);
+}
