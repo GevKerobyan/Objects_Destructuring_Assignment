97cee49b87062e069a9be86142a70ecbb1536604
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2021-12-15 19:04:48 +0400

    Initial commit

---
+++ classWork10_01.js
@@ -0,0 +1,7 @@
+// Get name, country and job using destructuring
+// let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+
+
+
+
+
---
+++ classWork10_06.js
@@ -0,0 +1,8 @@
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+
+const { name, country, job } = person;
+
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
+
